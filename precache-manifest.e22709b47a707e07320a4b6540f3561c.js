460c3701caa173493c468848a0f5227d77ca8f5e
Abdulrafayzahid <45996454+Abdulrafayzahid@users.noreply.github.com>
2019-12-04 23:08:02 +0500

    Updates

---
+++ precache-manifest.e22709b47a707e07320a4b6540f3561c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06f3be91b03b2aed3c383233dba0ccd",
+    "url": "/quizAppUsingReact/index.html"
+  },
+  {
+    "revision": "c17eb3de4c1924613bef",
+    "url": "/quizAppUsingReact/static/css/main.33a79945.chunk.css"
+  },
+  {
+    "revision": "c9a511b6e0e96eb6c298",
+    "url": "/quizAppUsingReact/static/js/2.78acdee7.chunk.js"
+  },
+  {
+    "revision": "c17eb3de4c1924613bef",
+    "url": "/quizAppUsingReact/static/js/main.9f6cbe96.chunk.js"
+  },
+  {
+    "revision": "cc1dd6bf4eef36fdbbdb",
+    "url": "/quizAppUsingReact/static/js/runtime-main.e62d4e04.js"
+  }
+]);
